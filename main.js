a17c6a14e8faac79a9114f22dbe6e32e2189876c
Yurtsiv Stepan <yurtsiv.stepan@gmail.com>
2018-12-12 21:57:13 +0100

    fixed indentation

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const settings = require('./settings.json');
 const bot = new TelegramBot(settings.api_token, {polling: true});
 
 const pickRandomElem = arr =>
- 	arr[Math.floor(Math.random() * arr.length)];
+  arr[Math.floor(Math.random() * arr.length)];
 
 const generateJoke = () => {
   const [part1, part2, part3, part4, part5, part6] = words.map(pickRandomElem)
