e2b6faeff1094b085e68f48a9a69ea282a1fef34
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2021-10-25 23:00:56 +0200

    add  polish jokes

--- src/bot.ts
+++ src/bot.ts
@@ -2,6 +2,7 @@ import Telegraf from 'telegraf';
 
 import regularWords from './words/regular';
 import apikoWords from './words/apiko';
+import polskaWords from './words/polska';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
@@ -10,14 +11,13 @@ console.log('Listenting');
 const pickRandomElem = arr =>
   arr[Math.floor(Math.random() * arr.length)];
 
-const generateJoke = (words) => {
-  const [part1, part2, part3, part4, part5, part6] = words.map(pickRandomElem)
-  return `${part1} приходить ${part2} i каже: "${part3}", а ${part4}: "${part5}". ${part6}.`;
-}
+const generateJoke = (words) =>
+  words.map(pickRandomElem).join(' ');
 
 const sendJokeWith = words => ({ reply }) => reply(generateJoke(words));
 
 bot.command('/regular_joke', sendJokeWith(regularWords));
 bot.command('/apiko_joke', sendJokeWith(apikoWords));
+bot.command('/polska_joke', sendJokeWith(polskaWords));
 
 bot.launch()
---
+++ src/words/polska.ts
@@ -0,0 +1,66 @@
+export default [
+  [
+    'Bartek',
+    'Kacper',
+    'Stepan',
+    'Klaudia',
+    'Robert',
+    'Marta',
+    'Artsiom',
+    'Vadim',
+    'Lina'
+  ],
+  ['przychodzi'],
+  [
+    'do Roberta',
+    'do Marty',
+    'do Kacpra',
+    'do Klaudii',
+    'do Vadima',
+    'do Liny',
+    'do Galerii Dominikańskiej',
+    'do Artsioma',
+    'do mamy Stepana',
+    'do Stepana',
+    'do Bartka z Martą',
+    'do polibudy',
+    'do swojej zmywarki',
+    'do Hanki'
+  ],
+  ['i mówi: "'],
+  [
+    '(nic)',
+    'jak tam, mordo?',
+    'jak tam twoje ośmiornicy?',
+    'masz ochotę na spacerek?',
+    'chodź, jebniemy piwka',
+    'chcę kupę',
+    'checesz kupę?',
+    'chodźmy do kina'
+  ],
+  ['", a'],
+  [
+    'głoś z nieba',
+    'w odpowiedź słyszy',
+    'wszyscy na to'
+  ],
+  [': "'],
+  [
+    'chcę kupę',
+    'debilem jesteś?',
+    'ja za',
+    'znowu?',
+    'nie!',
+    'jeb się',
+    'hoorray!!!',
+    'idź w dupę',
+    'zajebisty pomysł'
+  ],
+  ['".'],
+  [
+    'Wszyscy milczyli.',
+    'Cisza...',
+    'Wszyscy poszli do kina.',
+    'Marta poszła do domu.'
+  ]
+];
